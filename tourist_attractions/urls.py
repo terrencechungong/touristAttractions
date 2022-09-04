1db77c3d7263e9b4b80f6a61d301040d6ada3f74
Terrence Chungong <terrencechungong@gmail.com>
2022-09-03 19:04:03 -0700

    make user authentication work.

--- tourist_attractions/urls.py
+++ tourist_attractions/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
   path("", views.home, name="home"),
-  path("login/", views.loginn, name="login"),
+  path("login/", views.login_view, name="login"),
   path("register/", views.Register.as_view(), name="register"),
   path("details/<statename>", views.details, name="details"),
   path("attraction/create", views.AttractionCreate.as_view(), name="attractioncreate"),
